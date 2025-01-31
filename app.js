f45b6babc60a0e2e2208ecf9f145e5a9a11d9b7f
Gustavo Bueno <gusta.bueno02@gmail.com>
2025-01-30 22:19:34 -0300

    Finalizando JS

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-let listaDosAmigos = [];
-let listaDoSorteados = [];
-let numerador = 1;
+const friendsList = [];
 
+function adicionarAmigo() {
+  const friendName = document.querySelector(".input-name").value;
+
+  if (!friendName.trim() > 0) {
+    alert("Por favor, insira um nome válido.");
+    return;
+  }
+
+  renderFriend(friendName);
+}
+
+function renderFriend(friendName) {
+  const friendNameListItem = document.createElement("li");
+  const friendNameListContainer = document.querySelector(".name-list");
+  friendNameListContainer.appendChild(friendNameListItem);
+  document.querySelector(".input-name").value = ''; // reset input value on submit
+  
+  friendsList.push(friendName);
+
+  friendNameListItem.textContent = friendName;
+}
+
+function sortearAmigo() {
+  if (friendsList.length <= 3) {
+    alert("Por favor, adicione mais amigos para sortear.");
+    return;
+  }
+
+  const selectedFriend = document.querySelector(".result-list");
+  const selectedNumber = Math.floor(Math.random() * friendsList.length);
+  
+  selectedFriend.textContent = `${friendsList[selectedNumber]} foi sorteado(a)`;
+}
